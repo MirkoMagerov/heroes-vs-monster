9eab4a332059843690aa763fd0e87af7e6132008
miroslav.magerov <miroslav.magerov@gmail.com>
2023-12-13 16:38:51 +0100

    Creacion personajes acabada closes #3

--- heroes-vs-monster.cs
+++ heroes-vs-monster.cs
@@ -23,16 +23,79 @@ namespace Proyecto
             const int MinMonsterHealthValue = 7000, MaxMonsterHealthValue = 10000, MinMonsterAttackValue = 300, MaxMonsterAttackValue = 400, MinMonsterDefenseValue = 20, MaxMonsterDefenseValue = 30;
 
             // VARIABLES
-            // Cooldowns and special ability turns
-            int archerCooldown = 0, barbarianCooldown = 0, mageCooldown = 0, druidCooldown = 0;
-            int monsterKnockout = 0, barbarianAugmDefenseTurns = 0;
+            string archerName = "";
+            string barbarianName = "";
+            string mageName = "";
+            string druidName = "";
 
-            // Stats for every character
+            bool correctCreation;
+
+            // Special Variables
+            int archerCooldown = 0, barbarianCooldown = 0, mageCooldown = 0, druidCooldown = 0, monsterKnockout = 0, barbarianAugmDefenseTurns = 0;
+
+            // Stats
             float archerHealth = 0, ogArcherHealth = 0, archerAttack = 0, archerDefense = 0;
             float barbarianHealth = 0, ogBarbarianHealth = 0, barbarianAttack = 0, barbarianDefense = 0, ogBarbarianDefense;
             float mageHealth = 0, ogMageHealth = 0, mageAttack = 0, mageDefense = 0;
             float druidHealth = 0, ogDruidHealth = 0, druidAttack = 0, druidDefense = 0;
             float monsterHealth = 0, monsterAttack = 0, monsterDefense = 0;
+
+            // Bucle para creación de nombres de los personajes
+            do
+            {
+                correctCreation = GetCharactersNames(ref archerName, ref barbarianName, ref mageName, ref druidName);
+            } while (!correctCreation);
+        }
+
+        public static bool GetCharactersNames(ref string archerName, ref string barbName, ref string mageName, ref string druidName)
+        {
+            static string CapitalizeFirstLetter(string word)
+            {
+                return $"{word[0].ToString().ToUpper()}{word.Substring(1).ToLower()}";
+            }
+
+            string userInput;
+            string[] separatedNames;
+
+            Console.Write("Introduce 4 nombres separados por comas.\nSe asignarán a los personajes en este orden: Arquera, Bárbaro, Maga, Druida.\nEscribe aqui: ");
+            userInput = Console.ReadLine() ?? "";
+
+            userInput = userInput.Replace(" ", "");
+
+            separatedNames = userInput.Split(',');
+
+            for (int i = 0; i < separatedNames.Length; i++)
+            {
+                separatedNames[i] = CapitalizeFirstLetter(separatedNames[i]);
+
+                switch (i)
+                {
+                    case 0:
+                        archerName = separatedNames[i];
+                        break;
+
+                    case 1:
+                        barbName = separatedNames[i];
+                        break;
+
+                    case 2:
+                        mageName = separatedNames[i];
+                        break;
+
+                    case 3:
+                        druidName = separatedNames[i];
+                        break;
+                }
+            }
+
+            if (separatedNames.Length == 4)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
